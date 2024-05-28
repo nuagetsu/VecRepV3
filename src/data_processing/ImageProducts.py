Fix NCC calculation
Fixed NCC Calculation to allow for floating point values.

NCC calculation previously restricted to integers only which was correct for the previous implementation, but we now want to use floats.

src/data_processing/ImageProducts.py
@@ -39,8 +39,8 @@ def ncc(mainImg: NDArray, tempImg: NDArray) -> float:
     mainImg = np.pad(mainImg, max(len(mainImg), len(mainImg[0])),
                      'wrap')  # Padding the main image with wrapped values to simulate wrapping
 
-    mainImg = np.asarray(mainImg, np.uint8)  # Setting data types of array
-    tempImg = np.asarray(tempImg, np.uint8)
+    mainImg = np.asarray(mainImg, np.single)  # Setting data types of array
+    tempImg = np.asarray(tempImg, np.single)
 
     corr = cv2.matchTemplate(mainImg, tempImg, cv2.TM_CCORR_NORMED)
 

tests/data_processing/ImageGeneratorsTest.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from src.data_processing.ImageGenerators import get_image_set, get_binary_image_set
+from src.data_processing.ImageGenerators import get_image_set, get_binary_image_set, get_triangle_image_set
 
 class TestImageGenerators(unittest.TestCase):
     def test_output_shape(self):
@@ -30,5 +30,13 @@ class TestImageGenerators(unittest.TestCase):
             with self.subTest(imageType=imageType):
                 with self.assertRaises(ValueError):
                     get_image_set(imageType)
+
+    def test_get_triangle_image_set(self):
+        triangle_set = get_triangle_image_set()
+        values, counts = np.unique(triangle_set, return_counts=True, axis=0)
+        self.assertEqual(len(counts), 192)
+        triangle_set_mean_subtracted = get_triangle_image_set(mean_subtracted=True)
+        values_mean_subtracted, mean_subtracted_counts = np.unique(triangle_set_mean_subtracted,return_counts=True, axis=0)
+        self.assertEqual(len(mean_subtracted_counts), 192)
 if __name__ == '__main__':
     unittest.main()