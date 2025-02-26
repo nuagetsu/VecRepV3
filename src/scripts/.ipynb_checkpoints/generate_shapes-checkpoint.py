4a21c5f843370d75ae2f0542ad315f77021982d9
root <kimeunhe111@gmail.com>
2025-02-26 04:58:23 +0000

    Heavily optimised the function for generation of images

--- src/scripts/.ipynb_checkpoints/generate_shapes-checkpoint.py
+++ src/scripts/.ipynb_checkpoints/generate_shapes-checkpoint.py
@@ -11,26 +11,11 @@ from numpy.typing import NDArray
 from skimage.draw import polygon
 from src.data_processing.Filters import get_filtered_image_sets
 
-IMAGE_TYPES = ["NbinMmax_ones", "Nbin", "triangles", "triangle_mean_subtracted"]
-
-IMAGE_FILTERS = ["unique", "Nmax_ones", "one_island"]
-
-IMAGE_PRODUCT_TYPES = ["ncc", "ncc_scaled"]
-
-EMBEDDING_TYPES = ["pencorr_D"]
-
-dimensions = 32
-
-imageType = "shapes_3_dims_16_8"
-filters = ["100max_ones"]
-
-
-#get_image_set
 def parse_shapes_set(imageType: str, number=False):
     """
-    Parses the imageType string for an image set containing multiple shapes
-    :param imageType: imageType string
-    :return: Parameters for the whole image set
+    Parses the imageType string for an image set containing multiple shapes.
+    :param imageType: imageType string (e.g., "shapes_3_dims_16_4")
+    :return: Parameters for the whole image set (size, border_size, sides) and optionally number.
     """
     params = imageType.split("_")
     dims = False
@@ -52,52 +37,38 @@ def parse_shapes_set(imageType: str, number=False):
         return size, border_size, sides, number
     return size, border_size, sides
 
-def get_shapes_set(size: int, sides: int, border_size: int, num_images=float('inf'), filters=None):
+def generate_shapes(size: int, sides: int, border_size: int, num_images: int, unique: bool=False):
     """
-    Generates a set of shapes, either a fixed number or all permutations.
-
-    :param size: Size of shapes.
-    :param sides: Number of sides of shapes.
-    :param border_size: Size of the border.
-    :param num_images: Number of images to generate (set to float('inf') for all possible permutations).
-    :param filters: Filters to apply.
-    :return: A set of generated shape images.
+    Generator that yields one valid shape image at a time.
+    
+    :param size: Size of the shape grid.
+    :param sides: Number of sides of the shape.
+    :param border_size: Border size to pad the image.
+    :param num_images: Maximum number of images to yield.
+    :param unique: If True, only yield images from unique (translationally unique) combinations.
+    :yield: A valid shape image as a NumPy array.
     """
-    if filters is None:
-        filters = []
-    
-    unique = "unique" in filters
-    filter_copy = filters.copy()
-    all_permutations = set()
-    image_set = []
-    
     indexes = list(range(size ** 2))
+    seen = set() if unique else None
     count = 0
+    iterator = itertools.permutations(indexes, sides)
     
-    # If generating all possible permutations
-    if num_images == float('inf'):
-        iterator = itertools.permutations(indexes, sides)
-    else:
-        iterator = iter(lambda: random.sample(indexes, sides), None) 
-
     for comb in iterator:
         if count >= num_images:
-            break  
-
-        if unique and tuple(comb) in all_permutations:
-            continue
+            break
+        
+        if unique:
+            # normalized key to check for uniqueness
+            comb_key = tuple(sorted(comb))
+            if comb_key in seen:
+                continue
         
         image = np.zeros((size, size), dtype=int)
-        r = []
-        c = []
-        for index in comb:
-            r.append(index // size)
-            c.append(index % size)
-
-        # Check for straight lines
-        points = []
-        for i in range(0, len(r)):
-            points.append((r[i], c[i]))
+        r = [index // size for index in comb]
+        c = [index % size for index in comb]
+        
+        # check for collinearity in any three points
+        points = list(zip(r, c))
         collinear = False
         for coords in itertools.combinations(points, 3):
             m1 = (coords[1][1] - coords[0][1]) * (coords[2][0] - coords[1][0])
@@ -108,77 +79,81 @@ def get_shapes_set(size: int, sides: int, border_size: int, num_images=float('in
         if collinear:
             continue
 
-        # Check for intersecting lines within shape
+        # for shapes with more than 3 sides, check for intersecting lines
         if sides > 3:
             intersect = False
-            lines = []
-            for i in range(0, sides):
-                start = (r[i % sides], c[i % sides])
-                end = (r[(i + 1) % sides], c[(i + 1) % sides])
-                lines.append((start, end))
+            # Build list of edges for the shape
+            lines = [((r[i % sides], c[i % sides]), (r[(i + 1) % sides], c[(i + 1) % sides])) for i in range(sides)]
             for pair in itertools.combinations(lines, 2):
-                test = is_intersecting(pair[0][0], pair[0][1], pair[1][0], pair[1][1])
-                if test:
-                    intersect = test
+                if is_intersecting(pair[0][0], pair[0][1], pair[1][0], pair[1][1]):
+                    intersect = True
                     break
             if intersect:
                 continue
 
-        # Create shape
+        # fill the polygon
         rr, cc = polygon(r, c)
         image[rr, cc] = 1
-        image = np.pad(image, (border_size, border_size), constant_values=(0, 0))
-        image_set.append(image)
+        image = np.pad(image, border_size, constant_values=0)
         
-        # Accounts for all translationally unique combinations if required
+        # for unique filtering, need to mark the combination as seen 
         if unique:
-            all_permutations = add_permutations(all_permutations, comb, size)
-        
-        count += 1  # Increment counter
+            seen.add(comb_key)
+            
+        count += 1
+        yield image
 
-    if unique:
-        filter_copy.remove("unique")
-
-    image_set = np.array(image_set)
-    image_set = np.unique(image_set, axis=0)
-    image_set = get_filtered_image_sets(imageSet=image_set, filters=filter_copy)
-
-    return image_set
+def get_image_set(imageType: str, max_images=500, filters=None):
+    """
+    Generates the full image set based on imageType and filters.
     
-def get_image_set(imageType: str, max_images=None, filters=None):
-    size, border_size, sides = parse_shapes_set(imageType)
-    image_set = []
-    for j in sides:
-        image_set.extend(get_shapes_set(size, j, border_size, max_images).tolist())
-    image_set = np.array(image_set)
-    image_set = get_filtered_image_sets(imageSet=image_set, filters=filters)
+    :param imageType: String encoding the shapes and dimensions (e.g., "shapes_3_dims_16_4").
+    :param max_images: Maximum images per shape (or overall if you prefer).
+    :param filters: List of filters (e.g., ["unique"]).
+    :return: A NumPy array of filtered shape images.
+    """
+    size, border_size, sides_list = parse_shapes_set(imageType)
+    unique_flag = filters is not None and "unique" in filters
+
+    filters_remaining = filters.copy() if filters else []
+    # if unique_flag and "unique" in filters_remaining:
+    #     filters_remaining.remove("unique")
+    
+    all_images = []
+    for sides in sides_list:
+        for img in generate_shapes(size, sides, border_size, max_images, unique=unique_flag):
+            all_images.append(img)
+    
+    image_set = np.array(all_images)
+    print("got filter1")
+    image_set = get_filtered_image_sets(imageSet=image_set, filters=filters_remaining)
+    
     return image_set
 
-def append_to_npy(imageType: str, filters: List[str], imageSetFilepath: str, max_images=None) -> NDArray:
-    new_filtered_images = get_image_set(imageType=imageType, filters=filters, max_images=max_images)
+def append_to_npy(imageType: str, filters: list, imageSetFilepath: str, max_images=500):
+    """
+    Generates new filtered images and appends them to an existing .npy file,
+    using a more memory-efficient streaming approach.
+    
+    :param imageType: The image type string.
+    :param filters: List of filters to apply.
+    :param imageSetFilepath: Filepath for the .npy file.
+    :param max_images: Maximum images to generate per shape type.
+    :return: The updated array of images.
+    """
+    new_images = get_image_set(imageType=imageType, filters=filters, max_images=max_images)
     
     if os.path.exists(imageSetFilepath):
-        existing_images = np.load(imageSetFilepath, allow_pickle=True)  
-        updated_images = np.concatenate((existing_images, new_filtered_images), axis=0) 
+        existing_images = np.load(imageSetFilepath, allow_pickle=True)
+        # if large image set, can try saving in batches or using a memmap.
+        updated_images = np.concatenate((existing_images, new_images), axis=0)
     else:
-        updated_images = new_filtered_images 
-
+        updated_images = new_images
+    
     np.save(imageSetFilepath, updated_images)
-
+    
     return updated_images
 
-imageSet = append_to_npy(imageType, filters, 'filtered_images.npy', max_images=2000)
-
-# #np.array the dataset
-# imageSet = np.array(imageSet)
-
-# #matrixA then can be generated from any subset
-# percentage = 0
-# testSize = int(percentage * len(imageSet)) 
-# trainingSize = len(imageSet) - testSize
-# testSample, trainingSample = SamplingMethod.generate_random_sample(imageSet, testSize, trainingSize)
-
-# sampleName = f"{imageType} {filters} {percentage} sample"
-
-# sampleEstimator = SampleEstimator(sampleName=sampleName, trainingImageSet=trainingSample, embeddingType=embeddingType,
-#                                   imageProductType=imageProductType)
+imageType = "shapes_3_dims_16_4"
+filters = ["unique"]
+imageSet = append_to_npy(imageType, filters, 'filtered_images_24x24_eff.npy', max_images=10000)
