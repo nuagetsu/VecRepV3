6b8d26cced07019a6db7ccba382df32dccb33ef8
nuagetsu <ywernyen@D25004364.dsonet.corp.root>
2025-07-01 18:04:50 +0800

    add half of ncc ftt

--- evaluation/test_mtree.py
+++ evaluation/test_mtree.py
@@ -94,6 +94,36 @@ class CustomDataset(Dataset):
         
         return image.squeeze().to('cpu').numpy()
 
+class CustomDatasetMStar(Dataset):
+    def __init__(self, transform=None):
+        self.transform = transform
+
+        self.imgs_path = "/home/jovyan/data/mstar/Padded_imgs/"
+        file_list = glob.glob(self.imgs_path + "*")
+        self.data = []
+        for class_path in file_list:
+            class_name = class_path.split("/")[-1]
+            for img_path in glob.glob(class_path + "/*.JPG"):
+                self.data.append([img_path, class_name])
+        #print(self.data)
+        self.class_map = {"2S1" : 0, "BRDM_2": 1, "BTR_60": 2, "D7": 3, "SLICY": 4, "T62": 5, "ZIL131": 6, "ZSU_23_4": 7}
+
+    # Defining the length of the dataset
+    def __len__(self):
+        return len(self.data)
+
+    # Defining the method to get an item from the dataset
+    def __getitem__(self, index):
+        data_path = self.data[index]
+        image = Image.open(data_path[0])
+        image = transforms.functional.to_grayscale(image)
+        class_id = self.class_map[data_path[1]]
+        # Applying the transform
+        if self.transform:
+            image = self.transform(image)
+        
+        return image.squeeze().to('cpu').numpy(), class_id
+
 def get_data(size):
     transform = transforms.Compose([
     transforms.Resize((size, size)),
@@ -101,6 +131,15 @@ def get_data(size):
     transforms.Normalize((0.5,), (0.5,))
     ])
     return CustomDataset(transform)
+
+def get_data_MStar(size):
+    transform = transforms.Compose([
+    transforms.Resize((size, size)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+    ])
+    return CustomDatasetMStar(transform)
+
 
 def plot_data_mtree_ncc(x_axis="", title="", filename="", varied_arr=[], data1=[], data2=[], max_node_size=12):
     data_ncc = []
@@ -218,6 +257,73 @@ def mtree_ncc_query_sample_size(max_node_size=12, image_size=32, k=7, runs=2, sa
         print(f"Average runtime of mtree search: {avg_mtree:.6f} seconds")
     plot_data_mtree_ncc(x_axis="Sample size", title=f"Average runtime of finding {k} neighbours with image size {image_size} against sample sizes", filename="test_mtree_query_sample_sizes.png", 
                         varied_arr=sample_sizes, data1=avg_times_ncc, data2=avg_times_mtree, max_node_size=max_node_size)
+
+def mtree_ncc_query_sample_size_mstar(max_node_size=12, image_size=32, k=7, runs=2, sample_sizes=[1]):
+    total_time_ncc = 0
+    total_time_mtree = 0
+    avg_times_ncc = []
+    avg_times_mtree = []
+
+    data = get_data_MStar(image_size)
+
+    print(f"Average runtime of querying mtree and ncc for {k} NN over {runs} runs with image size {image_size} and max node size {max_node_size} and variable sample size")
+    for i in range(len(sample_sizes)):
+        print(f"NOW TRYING sample size: {sample_sizes[i]}")
+        total_time_ncc = 0
+        total_time_mtree = 0
+        sample_indices = random.sample(range(len(data)), sample_sizes[i])
+        sampled_test_data = Subset(data, sample_indices)
+
+        testSample = [item[0] for item in sampled_test_data]
+        
+
+        tree = getMTree(testSample, max_node_size)
+
+        # trans = transforms.Compose([transforms.Resize(img_sizes[i])])
+        # t_MNIST_data = trans(MNIST_data)
+
+        # for img in MNIST_data:
+        #     img = trans(img)
+
+        for _ in range(runs):
+            index1 = np.random.randint(len(data))
+            #input1=input_dataset[index1][0].squeeze().to('cpu')
+            unseen_image = data[index1][0]
+
+            start_time = time.time()
+            arr = []
+            for j in range(len(testSample)):
+                result = ImageProducts.ncc_scaled(testSample[j], unseen_image)
+                arr.append(result)
+            
+            unseen_img_arr = np.array(arr)
+            #print(unseen_img_arr)
+            imgProd_max_index = np.argpartition(unseen_img_arr, -(k+1))[-(k+1):]
+            end_time = time.time()
+
+            total_time_ncc += end_time - start_time
+
+            start_time = time.time()
+            imgs = getKNearestNeighbours(tree, unseen_image, k+1)
+            end_time = time.time()
+            total_time_mtree += end_time - start_time
+
+        avg_ncc = total_time_ncc / runs
+        avg_mtree = total_time_mtree / runs
+        avg_times_ncc.append(avg_ncc)
+        avg_times_mtree.append(avg_mtree)
+
+        # with open("./test_mtree_query_sample_sizes_avg_times_ncc_.txt", "w") as file:
+        #      file.write(f"avg times: {str(avg_times_ncc)}")
+        
+        # with open("test_mtree_query_sample_sizes_avg_times_mtree_.txt", "w") as file:
+        #      file.write(f"avg times: {str(avg_times_mtree)}")
+
+        print(f"Average runtime of ncc search: {avg_ncc:.6f} seconds")
+        print(f"Average runtime of mtree search: {avg_mtree:.6f} seconds")
+    # plot_data_mtree_ncc(x_axis="Sample size", title=f"Average runtime of finding {k} neighbours with image size {image_size} against sample sizes", filename="test_mtree_query_sample_sizes.png", 
+    #                     varied_arr=sample_sizes, data1=avg_times_ncc, data2=avg_times_mtree, max_node_size=max_node_size)
+
 
 def mtree_query_sample_size(max_node_size=12, image_size=16, k=7, runs=100, sample_sizes=[500000]):
     total_time_mtree = 0
@@ -349,6 +455,44 @@ def mtree_init_sample_size(max_node_size=12, image_size=32, k=7, runs=2, sample_
     
     plot_data_mtree_init(x_axis="Sample size", title=f"Average runtime of initialising mtree with image size {image_size}, max node size {max_node_size} against sample sizes", filename="test_mtree_init_sample_sizes.png", 
                         varied_arr=sample_sizes, data=avg_times_mtree_init, max_node_size=max_node_size)
+
+def mtree_init_sample_size_mstar(max_node_size=12, image_size=32, k=7, runs=2, sample_sizes=[1]):
+    total_time_mtree_init = 0
+    avg_times_mtree_init = []
+
+    data = get_data_MStar(image_size)
+
+    print(f"Average runtime of initialising mtree over {runs} runs with max node size {max_node_size} and image size {image_size} and variable sample sizes")
+
+    for i in range(len(sample_sizes)):
+        print(f"NOW TRYING sample size: {sample_sizes[i]}")
+        total_time_mtree_init = 0
+
+        sample_indices = random.sample(range(len(data)), sample_sizes[i])
+        sampled_test_data = Subset(data, sample_indices)
+
+        testSample = [item[0] for item in sampled_test_data]
+        #sampled_test_data[:,0]
+
+        for _ in range(runs):
+            start_time = time.time()
+            tree = getMTree(testSample, max_node_size)
+            end_time = time.time()
+            total_time_mtree_init += end_time - start_time
+            print(f"Finished one run of mtree init...")
+
+        
+        avg_mtree_init = total_time_mtree_init / runs
+        avg_times_mtree_init.append(avg_mtree_init)
+
+        
+        # with open("test_mtree_init_sample_sizes_avg_times_mtree_init.txt", "w") as file:
+        #     file.write(f"avg times: {str(avg_times_mtree_init)}")
+
+        print(f"Average runtime of mtree init: {avg_mtree_init:.6f} seconds")
+    
+    # plot_data_mtree_init(x_axis="Sample size", title=f"Average runtime of initialising mtree with image size {image_size}, max node size {max_node_size} against sample sizes", filename="test_mtree_init_sample_sizes.png", 
+    #                     varied_arr=sample_sizes, data=avg_times_mtree_init, max_node_size=max_node_size)
     
 
 # Get the array out into a file. Test out to make sure file writing works first.
@@ -470,7 +614,7 @@ def matG_init_img_size(image_sizes=[], k=7, runs=2, sample_size=5000):
 
 #def mtree_ncc_query_k(max_node_size=12, image_size=32, ks=[], runs=2, sample_size=5000):
 if __name__ == "__main__":
-    sample_sizes = [1000, 5000, 10000, 20000, 50000, 100000, 200000, 300000, 400000, 500000]
+    #sample_sizes = [1000, 5000, 10000, 20000, 50000, 100000, 200000, 300000, 400000, 500000]
     #image_sizes = [32, 64, 128, 140, 160, 180, 200]
     # mtree_ncc_query_sample_size(max_node_size=12, image_size=16, k=7, runs=3, sample_sizes=sample_sizes)
     # mtree_init_sample_size(max_node_size=12, image_size=16, k=7, runs=2, sample_sizes=sample_sizes)
@@ -478,7 +622,8 @@ if __name__ == "__main__":
     # matG_init_sample_size(image_size=16, k=7, runs=1, sample_sizes=[100, 500, 1000, 5000])
     # matG_init_img_size(image_sizes=image_sizes, k=7, runs=2, sample_size=50)
     #mtree_ncc_query_image_size(max_node_size=12, image_sizes=image_sizes, k=7, runs=100, sample_size=500)
-    mtree_query_sample_size(max_node_size=12, image_size=16, k=7, runs=100, sample_sizes=[500000])
+    mtree_ncc_query_sample_size_mstar(max_node_size=15, image_size=200, k=7, runs=10, sample_sizes=[100, 1000, 2000, 5000, 7000, 9465])
+    mtree_init_sample_size_mstar(max_node_size=15, image_size=200, k=7, runs=2, sample_sizes=[100, 1000, 2000, 5000, 9465])
 
 
 
